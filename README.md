f7e96f4e5e44f75833e496574c3d2ea52a742e1f
Bowie <marketing@letscode.com.br>
2024-01-23 18:54:55 -0300

    msg

--- README.md
+++ README.md
@@ -1,49 +1,11 @@
-![topideias logo](topideias.png)
+## OBJETIVO
 
-## Intern Test Internship Test Top Ideias Company
-
-Projeto Teste para Seleção de Estagiário - BackEnd/FrontEnd/PHP.
-
-
-### DESCRIÇÃO:
-
-
-    O teste consiste em observar a criatividade, organização, tempo e a lógica utilizada para resolver a tarefa proposta nesse teste. É um diferencial compreender e explicar cada linha de código inserido. Isso servirá para sua auto-análise, compreender o que está fazendo e poder dialogar possíveis outras soluções para o teste proposto no segundo momento da entrevista.
-    Good Coding :)
-
-
-## TAREFA:
-
-		Dada uma sequência de tamanho N (inteiro), determinar os números que compõem a sequência e o número de vezes que cada um deles ocorre na mesma.
-		Exemplo:
-		N = 6
-		1 5 3 4 5 1
-		* Saída:
-		1 ocorre 2 vezes
-		5 ocorre 2 vezes
-		3 ocorre 1 vez
-		4 ocorre 1 vez
+É projetado uma sequencia randomica(aleatória) de um vetor N (inteiro), determinando a quantidade de vezes que aparece cada numero do vetor.
+	
 
 ## INSTRUÇÕES:
 
-- Utilize uma versão mais atual do PHP.
-- Faça Fork desse projeto.
-- Crie uma branch apartir da branch `master` com o dia, mês, ano e seu primeiro nome. Exemplo: `test_06_08_2017_fulano`.
-- Trabalhe nessa branch crie os arquivos necessário para a solução do teste.
-- Faça um pull request do seu test.
-
-
-## EXTRA (OPCIONAL)
-
-	Esse extra é opcional. É um diferencial para observar outras habilidades que você tenha.  
-
-- Crie uma tela mais interativa para essa tarefa utilizando JS, HTML etc.
-- Fique à vontade em qual framework utilizar ou não.
-- Deixar tela responsivo também é legal :)
-
-
-
-
-Top Ideias Company
-
-[http://www.topideias.com.br](http://www.topideias.com.br)
+- Utilize a linguagem C#;
+- Faça fork desse projeto;
+- Crie uma branch apartir da branch 'test_06_08_2017_Nilson';
+- No projeto existem comentários que são explicações das linhas de codigos, das funcionalidades.
--- projeto/test5/Program.cs
+++ projeto/test5/Program.cs
@@ -10,41 +10,7 @@ namespace test5
     {
         static void Main(string[] args)
         {
-            //// Instancia que vai gerar valores randomicos
-            //Random random = new Random();
-            //// Lista de inteiros que vai receber os valores randomicos
-            //List<int> listaSelecionados = new List<int>();
-
-            //Console.Write("Digite o número de repetições desejado: ");
-
-            //// A variavel repeticoes vai receber o valor que for escrito no console e se a conversão do parse for feita com sucesso
-            //if (int.TryParse(Console.ReadLine(), out int repeticoes))
-            //{
-            //    // loop com a quantidade de vezes que o usuario quer
-            //    for (int i = 0; i < repeticoes; i++)
-            //    {
-            //        // randomNext vai gerar numeros no maximo até o valor que solicitou de repeticoes
-            //        int randomNumber = random.Next(repeticoes);
-            //        // Adiciona o valor gerado na lista
-            //        listaSelecionados.Add(randomNumber);
-            //    }
-
-            //    // Impressão de todos os valores gerados
-            //    Console.Write((listaSelecionados));
-            //    Console.Write("\n");
-
-            //    // loop pela quantidade de elementos da lista que é igual a de repeticoes
-            //    for (int i = 0; i < listaSelecionados.Count; i++)
-            //    {
-            //        // Quantidade de elementos repetido de acordo com o index da lista
-            //        int valoresRepetidos = listaSelecionados.Count(x => x == i);
-            //        // Definições da string final
-            //        string resultadoTexto = valoresRepetidos == 1 ? "vez" : "vezes";
-            //        // Mensagem concatenada por interpolarização
-            //        Console.WriteLine($"{i} ocorre {valoresRepetidos} {resultadoTexto}");
-            //    }
-            //}
-
+            
             // Instancia que vai gerar valores randomicos
             Random random = new Random();
             // Lista de inteiros que vai receber os valores randomicos
